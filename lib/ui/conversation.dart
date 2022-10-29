eddae1eb15183f5aa7c3dc4da1c790576f57c51b
SamiraRa <53578188+SamiraRa@users.noreply.github.com>
2022-10-29 09:27:07 +0600

    guidance ,chat, otp

--- lib/ui/conversation.dart
+++ lib/ui/conversation.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:guruji/ui/guidance.dart';
+import 'package:guruji/ui/homepage.dart';
+import 'package:guruji/ui/profile.dart';
 
 class ConversationScreen extends StatefulWidget {
   const ConversationScreen({super.key});
@@ -8,6 +11,40 @@ class ConversationScreen extends StatefulWidget {
 }
 
 class _ConversationScreenState extends State<ConversationScreen> {
+  int _currentSelected = 2;
+
+  void _onItemTapped(int index) async {
+    if (index == 1) {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => GuidanceScreen()));
+      setState(() {
+        _currentSelected = index;
+      });
+    }
+
+    if (index == 0) {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => HomePage()));
+      setState(() {
+        _currentSelected = index;
+      });
+    }
+    if (index == 2) {
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => ConversationScreen()));
+      setState(() {
+        _currentSelected = index;
+      });
+    }
+    if (index == 3) {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => ProfilePgae()));
+      setState(() {
+        _currentSelected = index;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,6 +62,36 @@ class _ConversationScreenState extends State<ConversationScreen> {
             Text("No Conversation")
           ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        onTap: _onItemTapped,
+        currentIndex: _currentSelected,
+        showUnselectedLabels: true,
+        unselectedItemColor: Colors.grey[800],
+        selectedItemColor: const Color.fromRGBO(10, 135, 255, 1),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: ImageIcon(AssetImage("assets/live guruji/home.png"),
+                color: Color(0xFF3A5A98), size: 20),
+          ),
+          BottomNavigationBarItem(
+            label: 'Guidance',
+            icon: ImageIcon(AssetImage("assets/live guruji/guidence.png"),
+                color: Color(0xFF3A5A98), size: 20),
+          ),
+          BottomNavigationBarItem(
+            label: 'Chat',
+            icon: ImageIcon(AssetImage("assets/live guruji/chat.png"),
+                color: Color(0xFF3A5A98), size: 20),
+          ),
+          BottomNavigationBarItem(
+            label: 'Me',
+            icon: ImageIcon(AssetImage("assets/live guruji/profile.png"),
+                color: Color(0xFF3A5A98), size: 20),
+          ),
+        ],
       ),
     );
   }
